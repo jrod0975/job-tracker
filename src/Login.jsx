d10d272e338d1477f0a8a103b90e7438ef1f62ca
Josue Rodriguez <jrod09752015@gmail.com>
2025-06-12 00:38:23 -0500

    responsive log in

--- src/Login.jsx
+++ src/Login.jsx
@@ -13,18 +13,21 @@ function Login({changeScreen}){
     return <div className="login-container">
        <div className="login-square gradient-border">
 
-        <div className="login-title">
+        <div className="login-title gradient-text">
             <h1>Job Tracker</h1>
         </div>
 
         <div className="login-fields">
-            <input className="user-field" type="text" />
-            <input className="password-field" type="text" />
+            <input className="user-field" type="text" placeholder="Username"/>
+            <input className="password-field" type="text" placeholder="Password"/>
+
+            <div className="login-button-field">
+                <button onClick={logInOnClick} className="login-button">Login</button>
+            </div>
+
         </div>
 
-        <div className="login-button-field">
-            <button className="login-button">Login</button>
-        </div>
+        
 
        </div>
     </div>
